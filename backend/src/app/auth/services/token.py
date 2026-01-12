1e89fc50abd8130b810c4d9e23e1276284325fee
asfqx <polinabrunina25@gmail.com>
2026-01-12 22:01:02 +0300

    feat: add core and auth utils

--- backend/src/app/auth/services/token.py
+++ backend/src/app/auth/services/token.py
@@ -2,6 +2,8 @@ from uuid import UUID
 from datetime import datetime, UTC, timedelta
 from typing import Any
 
+from fastapi import HTTPException, status
+
 from app.core import settings
 from app.security import JWTUtils
 
@@ -54,4 +56,25 @@ class JWTTokenService:
         try:
             payload = JWTUtils.decode(token)
         except:
+            raise HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail="Не удалось декодировать токен",
+            )
             
+        uuid = payload.get("sub")
+        
+        if uuid is None:
+                        raise HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail="Передан неверный или истекший токен",
+            )
+                        
+        exp = payload.get("exp")
+
+        if not exp or datetime.now(UTC).timestamp() > exp:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Переданный токен истек"
+            )
+            
+        return uuid
